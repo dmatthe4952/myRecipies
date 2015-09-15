03c88c523d9ead7ac641262362d065fbe3f8347c
dmatthe4952 <dmatthe@yahoo.com>
2015-09-15 20:59:51 +0000

    fix recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   belongs_to :chef
-  validates :title, presence: true,
+  validates :name, presence: true,
                     length: { minimum: 5 }
 end
